6e6b68b307972e1a224ab2c3e6e49044656d780d
Abiona Israel <israelabiona44@gmail.com>
2025-12-10 19:58:53 +0100

    Remove commented-out code for speech synthesis functionality

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-// let speech = new SpeechSynthesisUtterance();
-
-// document.querySelector("button").addEventListener("click", () =>{
-//   speech.text = document.querySelector("textarea").value;
-//   window.speechSynthesis.speak(speech);
-// })
-
 let speech = new SpeechSynthesisUtterance();
 
 let voices = [];
